2b5e599d9771c6d8206c1671b8420b9fa6baaad9
ECBelarmino <98925362+ECBelarmino@users.noreply.github.com>
2023-04-25 12:05:04 +0800

    Add files via upload

--- Practical_1.sql
+++ Practical_1.sql
@@ -10,8 +10,8 @@ CREATE TABLE Computer (
    DiskSize VARCHAR (15) NOT NULL,
    PRIMARY KEY (SerialNumber));
 
-INSERT INTO Computer VALUES (
-(9871234, 'HP', 'Pavilion 500-210qe', 'Intel i5-4530', 3.00, '6.0 Gbytes', '1.0 Tbytes'),
+SELECT*FROM Computer;
+INSERT INTO Computer (SerialNumber, Make, Model, ProcessorType, ProcessorSpeed, MainMemory, DiskSize) VALUES (9871234, 'HP', 'Pavilion 500-210qe', 'Intel i5-4530', 3.00, '6.0 Gbytes', '1.0 Tbytes'),
 (9871245, 'HP', 'Pavilion 500-210qe', 'Intel i5-4531', 3.00, '6.0 Gbytes', '1.0 Tbytes'),
 (9871256, 'HP', 'Pavilion 500-210qe', 'Intel i5-4532', 3.00, '6.0 Gbytes', '1.0 Tbytes'),
 (9871267, 'HP', 'Pavilion 500-210qe', 'Intel i5-4533', 3.00, '6.0 Gbytes', '1.0 Tbytes'),
@@ -22,7 +22,7 @@ INSERT INTO Computer VALUES (
 (6541003, 'Dell', 'OptiPlex 9022', 'Intel i7-4772', 3.00, '8.0 Gbytes', '1.0 Tbytes'),
 (6541004, 'Dell', 'OptiPlex 9023', 'Intel i7-4773', 3.00, '8.0 Gbytes', '1.0 Tbytes'),
 (6541005, 'Dell', 'OptiPlex 9024', 'Intel i7-4774', 3.00, '8.0 Gbytes', '1.0 Tbytes'),
-(6541006, 'Dell', 'OptiPlex 9025', 'Intel i7-4775', 3.00, '8.0 Gbytes', '1.0 Tbytes'));
+(6541006, 'Dell', 'OptiPlex 9025', 'Intel i7-4775', 3.00, '8.0 Gbytes', '1.0 Tbytes');
 
 SELECT*FROM Computer WHERE Make = 'HP';
 
